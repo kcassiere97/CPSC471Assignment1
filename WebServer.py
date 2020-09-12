745df41e86b0489dac4f1587f86b044988150c24
Kizar Cassiere <kcassiere97@gmail.com>
2020-09-12 13:26:38 -0700

    Add files via upload
    Updated Assignment

---
+++ WebServer.py
@@ -0,0 +1,41 @@
+#Kizar Cassiere 889991428
+from socket import *
+
+def main():
+
+    #declare server port
+    serverPort=2020
+    serverSocket = socket(AF_INET,SOCK_STREAM)
+
+    #Prepare a server socket
+    serverSocket.bind(('',serverPort))
+    serverSocket.listen(1)
+    print('the is running at:',serverPort)
+
+    while True:
+        #Establish the connection
+        print('Ready to serve')
+        connectionSocket, addr = serverSocket.accept()
+
+        try:
+            message = connectionSocket.recv(1024)
+            print (message, '::',message.split()[0],':',message.split()[1])
+            filename = message.split()[1]
+            print(filename,'||',filename[1:])
+            f = open(filename[1:])
+            outputdata = f.read()
+            print(outputdata)
+
+            #send HTTP request into socket
+            connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
+            connectionSocket.send(outputdata)
+            connectionSocket.close()
+
+        except IOError:
+            #IF connection fails throw error
+            connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
+    serverSocket.close()
+
+if __name__ == "__main__":
+    main()
+
